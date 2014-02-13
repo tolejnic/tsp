097c34c0c6cfcdcc01c9f4ece6c232d03617bbd4
tim <tim@snos-MacBook.local>
2014-02-13 13:55:54 -0800

    first commit of tsp

---
+++ models/menu.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## Customize your APP title, subtitle and menus here
+#########################################################################
+
+response.title = request.application
+response.subtitle = T('customize me!')
+
+## read more at http://dev.w3.org/html5/markup/meta.name.html
+response.meta.author = 'Your Name <you@example.com>'
+response.meta.description = 'a cool new app'
+response.meta.keywords = 'web2py, python, framework'
+response.meta.generator = 'Web2py Web Framework'
+response.meta.copyright = 'Copyright 2011'
+
+## your http://google.com/analytics id
+response.google_analytics_id = None
+
+#layout 
+response.pagename = request.function.capitalize()
+response.tagline = "My awesome web2py application"
+response.brand = request.application.capitalize()
+response.brand_url = URL('default','index')
+response.alerttype = 'warning' #info, error, success, warning
+#########################################################################
+## this is the main application menu add/remove items as required
+#########################################################################
+
+response.menu = [
+    (T('Home'), False, URL('default','index'), []),
+    (T('About Me'),False, URL('default','about_me'), []),
+    (T('Examples'),False, URL('default','examples'), []),
+    (T('Pricing'),False, URL('default','pricing'), [])
+    ]
+
+if auth.user:
+    response.auth_menu = [
+    (T('Profile'), False, URL('default','user',args=['profile']), []),
+    (T('Logout'), False, URL('default','user',args=['logout']), [])
+    ]
+else:
+    response.auth_menu = [
+    (T('Login'), False, URL('default','user',args='login'), [])
+    ]
+
+
+#########################################################################
+## provide shortcuts for development. remove in production
+#########################################################################
+
+def _():
+    # shortcuts
+    app = request.application
+    ctr = request.controller    
+    # useful links to internal admin and appadmin pages
+    response.menu+=[
+ 
+         ]
+_()
